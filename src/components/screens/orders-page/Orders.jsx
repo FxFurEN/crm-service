93099469fbaafca2cc2b4d2c3eaadd6d6f7c80b6
FxFurEN <nikitostimoshenko@gmail.com>
2024-01-08 02:43:30 +0300

    add table in Order page

--- src/components/screens/orders-page/Orders.jsx
+++ src/components/screens/orders-page/Orders.jsx
@@ -1,4 +1,5 @@
 import '../../../assets/styles/main.css';
+import style from  '../../../assets/styles/table.module.css';
 import '../../../assets/styles/global.css';
 
 
@@ -6,6 +7,61 @@ const Orders = () =>{
     return(
         <main id="main">
             <h2>Заказы</h2>
+                <div className={style.wrapper}>
+                
+                    <div className={style.table}>
+                        
+                        <div className={style.row + ' ' + style.header}>
+                            <div className={style.cell}>
+                                Заказ
+                            </div>
+                            <div className={style.cell}>
+                                Обновлен
+                            </div>
+                            <div className={style.cell}>
+                                Статус
+                            </div>
+                            <div className={style.cell}>
+                                Срок
+                            </div>
+                            <div className={style.cell}>
+                                Клиент
+                            </div>
+                            <div className={style.cell}>
+                                Исполнитель
+                            </div>
+                            <div className={style.cell}>
+                                Наименование услуги
+                            </div>
+                            
+                        </div>
+                        
+                        <div className={style.row}>
+                            <div className={style.cell} data-title="Номер">
+                                1
+                            </div>
+                            <div className={style.cell} data-title="Обновлен">
+                                07.01
+                            </div>
+                            <div className={style.cell} data-title="Статус">
+                                В работе
+                            </div>
+                            <div className={style.cell} data-title="Срок">
+                                3 дн
+                            </div>
+                            <div className={style.cell} data-title="Клиент">
+                                Рома Аегис Хиро
+                            </div>
+                            <div className={style.cell} data-title="Исполнитель">
+                                Тикита Нимошенко
+                            </div>
+                            <div className={style.cell} data-title="Наименование услуги">
+                                Ремонт ноутбука
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
         </main>
     )
 }
