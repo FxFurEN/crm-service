30fa45ac0fb30138f722fc3855b14cf7f4868dae
FxFurEN <nikitostimoshenko@gmail.com>
2024-01-10 14:58:14 +0300

    add navigation in nav-line

--- src/components/screens/navbar/Navbar.jsx
+++ src/components/screens/navbar/Navbar.jsx
@@ -2,9 +2,61 @@ import './navbar.css';
 import '../../../assets/styles/global.css';
 import { IonIcon } from '@ionic/react';
 import { homeOutline, peopleOutline, settingsOutline, diamondOutline, appsOutline, barChartOutline, archiveOutline, personOutline } from 'ionicons/icons';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  const location = useLocation();
+  const currentPage = location.pathname;
+
+  const russianPageNames = new Map([
+    ['/home', 'Главная'],
+    ['/clients', 'Клиенты'],
+    ['/report', 'Отчет'],
+    ['/orders', 'Заказы'],
+    ['/inventory', 'Склад'],
+    ['/settings', 'Настройки'],
+    ['/settings/profile', 'Профиль'],
+    ['/settings/documents', 'Документы'],
+    ['/settings/integrations', 'Интеграции'],
+    ['/settings/general/company', 'Компания'],
+    ['/settings/general/orders', 'Общие заказы'],
+    ['/settings/statuses', 'Статусы'],
+    ['/settings/services', 'Услуги'],
+    ['/settings/notifications', 'Уведомления'],
+    ['/settings/reference/accounts', 'Счета'],
+    ['/settings/paymentMethods', 'Способы оплаты'],
+    ['/settings/fields/order', 'Поля заказов'],
+    ['/settings/fields/client', 'Поля клиентов'],
+    ['/settings/orderType', 'Типы заказов'],
+    ['/settings/clientType', 'Типы клиентов'],
+    ['/settings/handbooks', 'Справочники'],
+  ]);
+  const additionalPageNames = new Map([
+    ['/settings/general', 'Общие'],
+    ['/settings/fields', 'Поле'],
+    ['/settings/reference', 'Справочники'],
+  ]);
+
+  const getPageSpans = (path) => {
+    const pathParts = path.split('/').filter(part => part !== ''); 
+    let currentPath = '';
+    return pathParts.map((part, index) => {
+      currentPath += `/${part}`;
+      const pageName = russianPageNames.get(currentPath) || additionalPageNames.get(currentPath);
+      const isClickable = russianPageNames.has(currentPath) || currentPath === '/settings';
+      return (
+        <span key={index}>
+          {isClickable ? (
+            <Link to={currentPath} style={{ cursor: 'pointer' }}>{pageName || part}</Link>
+          ) : (
+            <span style={{ cursor: 'text' }}>{pageName !== undefined ? pageName : part}</span>
+          )}
+          {index < pathParts.length - 1 && <span> /</span>} {}
+        </span>
+      );
+    });
+  };
+
   return (
     <>
       <nav id="navbar">
@@ -67,6 +119,11 @@ const Navbar = () => {
       </nav>
       <div className='nav-line'>
         <div className="wrapper">
+        <li className="navbar-item flexbox-left hover-navbar" style={{ paddingLeft: '5em' }}>
+          <Link to={currentPage}>
+            {getPageSpans(currentPage)}
+          </Link>
+          </li> 
           <ul className="nav-links">
           <li className="navbar-item flexbox-left">
             <Link to='/settings/profile' className="navbar-item-inner flexbox-left">
