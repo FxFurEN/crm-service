660652462569e2d731d2c1f8c78b04ed7b349385
FxFurEN <nikitostimoshenko@gmail.com>
2024-01-17 18:56:20 +0300

    add searchBox and some buttons

--- src/components/screens/orders-page/Orders.jsx
+++ src/components/screens/orders-page/Orders.jsx
@@ -1,14 +1,37 @@
+import { IonButton, IonIcon,  } from '@ionic/react';
+import {filterOutline, cloudUploadOutline, cloudDownloadOutline } from 'ionicons/icons';
+
 import '../../../assets/styles/main.css';
 import style from  '../../../assets/styles/table.module.css';
 import '../../../assets/styles/global.css';
+import '../../../assets/styles/addbutton.css';
+import '../../../assets/styles/button-ion.css';
+
+import AddButton from '../addButton/AddButton';
+import SearchBox from '../searchBox/SearchBox';
+
 
 
 const Orders = () =>{
     return(
         <main id="main">
+            <div>
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                    <SearchBox/>
+                    <IonButton fill="clear">
+                        <IonIcon slot="icon-only" color="white" icon={filterOutline}></IonIcon>
+                    </IonButton>
+                    <IonButton fill="clear">
+                        <IonIcon slot="icon-only" icon={cloudUploadOutline}></IonIcon>
+                    </IonButton>
+                    <IonButton fill="clear">
+                        <IonIcon slot="icon-only" icon={cloudDownloadOutline}></IonIcon>
+                    </IonButton>
+                </div>    
+            </div>
+            <div>
                 <div className={style.wrapper}>
-                
-                <div className={style.table} style={{ boxShadow: '0 10px 13px 2px rgba(0, 0, 0, 0.2)' }}>
+                    <div className={style.table} style={{ boxShadow: '0 10px 13px 2px rgba(0, 0, 0, 0.2)' }}>
                         
                         <div className={style.row + ' ' + style.header}>
                             <div className={style.cell}>
@@ -61,6 +84,11 @@ const Orders = () =>{
                         
                     </div>
                 </div>
+            </div>
+            <div>
+                <AddButton/>
+            </div>
+                
         </main>
     )
 }
