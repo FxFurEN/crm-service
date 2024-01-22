31616876b3089f6da0244ef0a60359e47bbd724b
fxfuren <nikitostimoshenko@gmail.com>
2024-01-22 12:31:00 +0300

    add some buttons

--- src/components/screens/inventory-page/Inventory.jsx
+++ src/components/screens/inventory-page/Inventory.jsx
@@ -1,8 +1,10 @@
-import { IonButton, IonIcon,  } from '@ionic/react';
-import {filterOutline, cloudUploadOutline, cloudDownloadOutline } from 'ionicons/icons';
+import { IonButton, IonIcon } from '@ionic/react';
+import {filterOutline, cloudUploadOutline, cloudDownloadOutline, add } from 'ionicons/icons';
+
 
 import "../navbar/navbar.css";
 import '../../../assets/styles/global.css';
+import style from  '../../../assets/styles/table.module.css';
 
 import SearchBox from '../../../components/screens/searchBox/SearchBox';
 import AddButton from '../addButton/AddButton';
@@ -10,22 +12,38 @@ import AddButton from '../addButton/AddButton';
 const Inventory = () =>{
     return(
         <main id="main">
-            <div>
-                <div>a
+            <div className={style.table}>
+                    <div className={style.row}>
+                        <div className={style.column}>
+                            <div className={style.row}>
+                                <div className={style.column} style={{width: "100%"}}>
+                                    <p>Все категории</p>
+                                </div>
+                                <div className={style.column}>
+                                    <IonButton fill="clear">
+                                        <IonIcon slot="icon-only" color="white" icon={add}></IonIcon>
+                                    </IonButton> 
+                                </div>
+                            </div>
+                            
+                                                                                                                                                                                                                                                           
+                        </div>
+                        <div className={style.column} style={{width: "80%"}}>
+                            <div style={{display: "flex", justifyContent: "space-between"}}>
+                                <SearchBox  />
+                                <IonButton fill="clear">
+                                    <IonIcon slot="icon-only" color="white" icon={filterOutline}></IonIcon>
+                                </IonButton>
+                                <IonButton fill="clear">
+                                    <IonIcon slot="icon-only" icon={cloudUploadOutline}></IonIcon>
+                                </IonButton>
+                                <IonButton fill="clear">
+                                    <IonIcon slot="icon-only" icon={cloudDownloadOutline}></IonIcon>
+                                </IonButton>
+                            </div>                                                                                                                                                                                                                                                
+                        </div>
+                    </div>
                 </div>
-                <div style={{display: "flex", justifyContent: "space-between"}}>
-                    <SearchBox  />
-                    <IonButton fill="clear">
-                        <IonIcon slot="icon-only" color="white" icon={filterOutline}></IonIcon>
-                    </IonButton>
-                    <IonButton fill="clear">
-                        <IonIcon slot="icon-only" icon={cloudUploadOutline}></IonIcon>
-                    </IonButton>
-                    <IonButton fill="clear">
-                        <IonIcon slot="icon-only" icon={cloudDownloadOutline}></IonIcon>
-                    </IonButton>
-                </div>    
-            </div>
             <div>
 
             </div>
