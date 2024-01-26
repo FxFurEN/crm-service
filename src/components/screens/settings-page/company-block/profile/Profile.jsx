643024f08bb963e2e6abe42c44450ae2b1667a8c
FxFurEN <nikitostimoshenko@gmail.com>
2024-01-26 15:01:45 +0300

    add layout Profile

--- src/components/screens/settings-page/company-block/profile/Profile.jsx
+++ src/components/screens/settings-page/company-block/profile/Profile.jsx
@@ -1,9 +1,49 @@
 import '../../../../../assets/styles/global.css';
 import '../../../../../assets/styles/main.css';
+import '../../../../../assets/styles/ion-style.css';
+
+import { IonItemGroup, IonItem, IonInput, IonButton } from '@ionic/react';
 
 const Profile = () =>{
     return(
         <main id="main">
+            <>
+                <IonItemGroup>
+                    <IonItem >
+                        <IonInput 
+                            label="ФИО" 
+                            labelPlacement="stacked" 
+                            class="custom"
+                        ></IonInput>
+                    </IonItem>
+                    <IonItem >
+                        <IonInput 
+                            label="Телефн" 
+                            labelPlacement="stacked" 
+                            class="custom"
+                        ></IonInput>
+                    </IonItem>
+                    <IonItem >
+                        <IonInput 
+                            label="Почта" 
+                            labelPlacement="stacked" 
+                            class="custom"
+                        ></IonInput>
+                    </IonItem>
+                    <IonButton class="custom">Сохранить</IonButton>
+                </IonItemGroup>
+                <IonItemGroup>
+                    <IonItem >
+                        <IonInput 
+                            label="Новый пароль" 
+                            labelPlacement="stacked" 
+                            class="custom"
+                        ></IonInput>
+                    </IonItem>
+                    <IonButton class="custom">Сохранить</IonButton>
+                </IonItemGroup>
+            </>
+           
         </main>
     )
 }
