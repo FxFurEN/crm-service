c0a1381ccfe4ceb7d542ef586e49f739dce5436e
fxfuren <nikitostimoshenko@gmail.com>
2024-01-27 22:27:30 +0300

    add layout documents

--- src/components/screens/settings-page/company-block/documents/Documents.jsx
+++ src/components/screens/settings-page/company-block/documents/Documents.jsx
@@ -1,9 +1,41 @@
 import '../../../../../assets/styles/global.css';
 import '../../../../../assets/styles/main.css';
+import { IonItem, IonLabel, IonItemGroup, IonItemDivider } from '@ionic/react';
+import { caretForwardOutline } from 'ionicons/icons';
 
 const Documents = () =>{
     return(
         <main id="main">
+            <IonItemGroup>
+                <IonItemDivider>
+                        <IonLabel>Заказы</IonLabel>
+                </IonItemDivider>
+                <IonItem button detail={true} detailIcon={caretForwardOutline}>
+                    <IonLabel>Акт выполненных работ</IonLabel>
+                </IonItem>
+                <IonItem button detail={true} detailIcon={caretForwardOutline}>
+                    <IonLabel>Гарантийная квитация</IonLabel>
+                </IonItem>
+                <IonItem button detail={true} detailIcon={caretForwardOutline}>
+                    <IonLabel>Приемная квитация</IonLabel>
+                </IonItem>
+            </IonItemGroup>
+            <IonItemGroup>
+                <IonItemDivider>
+                        <IonLabel>Магазин</IonLabel>
+                </IonItemDivider>
+                <IonItem button detail={true} detailIcon={caretForwardOutline}>
+                    <IonLabel>Товарный чек</IonLabel>
+                </IonItem>
+            </IonItemGroup>
+            <IonItemGroup>
+                <IonItemDivider>
+                        <IonLabel>Склад</IonLabel>
+                </IonItemDivider>
+                <IonItem button detail={true} detailIcon={caretForwardOutline}>
+                    <IonLabel>QR-код</IonLabel>
+                </IonItem>
+            </IonItemGroup>
         </main>
     )
 }
