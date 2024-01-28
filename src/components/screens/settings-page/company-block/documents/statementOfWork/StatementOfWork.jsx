8b372aafb94be9ab1ae6b1bec1c84203704bb441
fxfuren <nikitostimoshenko@gmail.com>
2024-01-28 21:15:15 +0300

    add Tiny editor

--- src/components/screens/settings-page/company-block/documents/statementOfWork/StatementOfWork.jsx
+++ src/components/screens/settings-page/company-block/documents/statementOfWork/StatementOfWork.jsx
@@ -1,10 +1,28 @@
 import '../../../../../../assets/styles/global.css';
 import '../../../../../../assets/styles/main.css';
+import { Editor } from '@tinymce/tinymce-react';
+import env from "react-dotenv";
 
 const StatementOfWork = () =>{
+    const apiKey = env.API_KEY;
+
     return(
         <main id='main' style={{color: 'black'}}>
-                
+           <Editor
+                apiKey={apiKey}
+                init={{
+                    plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+                    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                    tinycomments_mode: 'embedded',
+                    tinycomments_author: 'Author name',
+                    mergetags_list: [
+                    { value: 'First.Name', title: 'First Name' },
+                    { value: 'Email', title: 'Email' },
+                    ],
+                    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+                }}
+                initialValue="Welcome to TinyMCE!"
+            />          
         </main>
     )
 }
