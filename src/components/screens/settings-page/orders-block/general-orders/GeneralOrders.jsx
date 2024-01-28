1f51318809efaf3d3bc551045cc387efc67005a5
fxfuren <nikitostimoshenko@gmail.com>
2024-01-28 14:44:34 +0300

    add layout GeneralOrders

--- src/components/screens/settings-page/orders-block/general-orders/GeneralOrders.jsx
+++ src/components/screens/settings-page/orders-block/general-orders/GeneralOrders.jsx
@@ -1,9 +1,32 @@
 import '../../../../../assets/styles/global.css';
 import '../../../../../assets/styles/main.css';
+import { IonItemGroup, IonItem, IonInput, IonButton } from '@ionic/react';
 
 const GeneralOrders = () =>{
     return(
         <main id="main">
+            <>
+                <IonItemGroup>
+                    <IonItem class="custom" >
+                        <IonInput 
+                            label="Срок по умолчанию, дн." 
+                            labelPlacement="stacked" 
+                            class="custom-input"
+                        ></IonInput>
+                    </IonItem>
+                    <IonItem class="custom">
+                        <IonInput 
+                            label="Гарантия по умолчанию, дн." 
+                            labelPlacement="stacked" 
+                            class="custom-input"
+                        ></IonInput>
+                    </IonItem>
+                    <IonItem class="custom" >
+                        <IonButton class="custom">Сохранить</IonButton>
+                    </IonItem>
+                    
+                </IonItemGroup>
+            </>
         </main>
     )
 }
