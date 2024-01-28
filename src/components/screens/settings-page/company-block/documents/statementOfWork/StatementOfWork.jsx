0b03b44ec02b424f1e56915734b19329dd17bbed
fxfuren <nikitostimoshenko@gmail.com>
2024-01-28 21:06:40 +0300

    cut CKeditor

--- src/components/screens/settings-page/company-block/documents/statementOfWork/StatementOfWork.jsx
+++ src/components/screens/settings-page/company-block/documents/statementOfWork/StatementOfWork.jsx
@@ -1,30 +1,11 @@
 import '../../../../../../assets/styles/global.css';
 import '../../../../../../assets/styles/main.css';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const StatementOfWork = () =>{
     return(
-        <div className="main">
-                <h2>Using CKEditor&nbsp;5 build in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor&nbsp;5!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event ) => {
-                        console.log( event );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-        </div>
+        <main id='main' style={{color: 'black'}}>
+                
+        </main>
     )
 }
 export default StatementOfWork;
