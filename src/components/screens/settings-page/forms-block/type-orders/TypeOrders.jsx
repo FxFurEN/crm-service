dd0a9fe6a5b2c3f5b9a7a4c4d1ceb3f7ab44e9aa
fxfuren <nikitostimoshenko@gmail.com>
2024-01-28 14:46:42 +0300

    add layou TypeOrders

--- src/components/screens/settings-page/forms-block/type-orders/TypeOrders.jsx
+++ src/components/screens/settings-page/forms-block/type-orders/TypeOrders.jsx
@@ -1,9 +1,30 @@
+import {useState } from 'react';
+import { IonItem, IonLabel, IonList, IonReorder, IonReorderGroup, } from '@ionic/react';
+
 import '../../../../../assets/styles/global.css';
 import '../../../../../assets/styles/main.css';
 
 const TypeOrders = () =>{
+    const [items, setItems] = useState([1, 2, 3]);
+
+  function handleReorder(event) {
+    console.log('Before complete', items);
+    setItems(event.detail.complete(items));
+    console.log('After complete', items);
+  }
+
     return(
         <main id="main">
+            <IonList style={{ background: 'hsl(var(--background))'}}>
+                <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
+                    {items.map((item) => (
+                    <IonItem key={item}>
+                        <IonLabel>Item {item}</IonLabel>
+                        <IonReorder slot="end"></IonReorder>
+                    </IonItem>
+                    ))}
+                </IonReorderGroup>
+            </IonList>
         </main>
     )
 }
