c12d6aaeb0cccaab339fb6da4f301638618a100d
fxfuren <nikitostimoshenko@gmail.com>
2024-01-28 22:13:43 +0300

    translate editor

--- src/components/screens/settings-page/company-block/documents/statementOfWork/StatementOfWork.jsx
+++ src/components/screens/settings-page/company-block/documents/statementOfWork/StatementOfWork.jsx
@@ -7,11 +7,13 @@ const StatementOfWork = () =>{
         <main id='main' style={{color: 'black'}}>
            <Editor
                 apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
+               
                 init={{
                     plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
                     toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
                     tinycomments_mode: 'embedded',
                     tinycomments_author: 'Author name',
+                    language: 'ru',
                     mergetags_list: [
                     { value: 'First.Name', title: 'First Name' },
                     { value: 'Email', title: 'Email' },
