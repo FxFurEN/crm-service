d8a6f50a0e307b517b8fe88f6dd6f382090da6cd
FxFurEN <nikitostimoshenko@gmail.com>
2024-01-29 13:17:29 +0300

    add layout HandBooks

--- src/components/screens/settings-page/forms-block/handbooks/Handbooks.jsx
+++ src/components/screens/settings-page/forms-block/handbooks/Handbooks.jsx
@@ -1,9 +1,33 @@
-import '../../../../../assets/styles/global.css';
-import '../../../../../assets/styles/main.css';
+import SearchBox from '../../../searchBox/SearchBox';
+import { IonItem, IonItemGroup, IonLabel, IonIcon } from '@ionic/react';
+import { pencilOutline } from 'ionicons/icons';
+
 
 const Handbooks = () =>{
+    const data = [
+        { name: '1'  },
+        { name: '2'}
+      ];
+
     return(
         <main id="main">
+            <div>
+                <SearchBox />
+            </div>
+            <div>
+            <IonItemGroup>
+                <IonItem sizr='auto'>
+                    <IonLabel>Имя</IonLabel>
+                    <IonLabel style={{textAlign: 'right'}}>Действие</IonLabel>
+                </IonItem>
+                 {data.map((item, index) => (
+                    <IonItem button key={index}>
+                        <IonLabel>{item.name}</IonLabel>
+                        <IonIcon icon={pencilOutline} color="white" style={{textAlign: 'right'}} />
+                    </IonItem>
+                ))}
+            </IonItemGroup>
+            </div>
         </main>
     )
 }
