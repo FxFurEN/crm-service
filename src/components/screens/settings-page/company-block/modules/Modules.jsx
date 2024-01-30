e4ab251fff0c6c4b7f7db764580d7afd5940464e
FxFurEN <nikitostimoshenko@gmail.com>
2024-01-30 13:34:04 +0300

    finish fix modules page

--- src/components/screens/settings-page/company-block/modules/Modules.jsx
+++ src/components/screens/settings-page/company-block/modules/Modules.jsx
@@ -1,13 +1,35 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { IonButton, IonGrid, IonItem, IonItemGroup, IonToggle } from "@ionic/react";
-import { setVisibility, selectVisibility } from '../../../../../redux/visibilitySlice'; // Замените на правильный путь
+import { setVisibility, selectVisibility } from '../../../../../redux/visibilitySlice';
+import { useState } from 'react';
 
 const Modules = () => {
   const dispatch = useDispatch();
   const { isSkladVisible, isMagazinVisible, isOrdersVisible } = useSelector(selectVisibility);
 
+  // Временное состояние, которое будет обновляться по мере изменения тумблеров
+  const [tempVisibility, setTempVisibility] = useState({
+    isSkladVisible,
+    isMagazinVisible,
+    isOrdersVisible,
+  });
+
+  // Обработчики изменения тумблеров
+  const handleSkladToggle = () => {
+    setTempVisibility(prev => ({ ...prev, isSkladVisible: !prev.isSkladVisible }));
+  };
+
+  const handleMagazinToggle = () => {
+    setTempVisibility(prev => ({ ...prev, isMagazinVisible: !prev.isMagazinVisible }));
+  };
+
+  const handleOrdersToggle = () => {
+    setTempVisibility(prev => ({ ...prev, isOrdersVisible: !prev.isOrdersVisible }));
+  };
+
+  // Обработчик сохранения
   const handleSaveClick = () => {
-    dispatch(setVisibility({ isSkladVisible, isMagazinVisible, isOrdersVisible }));
+    dispatch(setVisibility(tempVisibility));
   };
 
   return (
@@ -15,16 +37,24 @@ const Modules = () => {
       <IonGrid>
         <IonItemGroup>
           <IonItem>
-            <IonToggle checked={isSkladVisible} onIonChange={() => dispatch(setVisibility({ isSkladVisible: !isSkladVisible }))}>Склад</IonToggle>
+            <IonToggle checked={tempVisibility.isSkladVisible} onIonChange={handleSkladToggle}>
+              Склад
+            </IonToggle>
           </IonItem>
           <IonItem>
-            <IonToggle checked={isMagazinVisible} onIonChange={() => dispatch(setVisibility({ isMagazinVisible: !isMagazinVisible }))}>Магазин</IonToggle>
+            <IonToggle checked={tempVisibility.isMagazinVisible} onIonChange={handleMagazinToggle}>
+              Магазин
+            </IonToggle>
           </IonItem>
           <IonItem>
-            <IonToggle checked={isOrdersVisible} onIonChange={() => dispatch(setVisibility({ isOrdersVisible: !isOrdersVisible }))}>Заказы</IonToggle>
+            <IonToggle checked={tempVisibility.isOrdersVisible} onIonChange={handleOrdersToggle}>
+              Заказы
+            </IonToggle>
           </IonItem>
           <IonItem>
-            <IonButton className="custom" onClick={handleSaveClick}>Сохранить</IonButton>
+            <IonButton className="custom" onClick={handleSaveClick}>
+              Сохранить
+            </IonButton>
           </IonItem>
         </IonItemGroup>
       </IonGrid>
