1fa27d47aac5ff87072d4c630e1ef0f3927cc298
FxFurEN <nikitostimoshenko@gmail.com>
2024-02-01 13:29:04 +0300

    update modal empoleyes and add style modal

--- src/components/screens/settings-page/company-block/employees/addEmpoyees/EmployeesModal.jsx
+++ src/components/screens/settings-page/company-block/employees/addEmpoyees/EmployeesModal.jsx
@@ -1,11 +1,39 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonModal, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react"
-import { useRef } from "react";
-import SearchBox from "../../../../searchBox/SearchBox";
+import { IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonItemDivider, IonLabel, IonModal, IonSearchbar, IonText, IonTitle, IonToolbar } from "@ionic/react"
+import { useRef, useState, useEffect } from "react";
+
 import '../../../../../../assets/styles/ion-style.css';
 
 const EmployeesModal = ({ isOpen, onClose}) =>{
     const modal = useRef(null);
+    const [invitationCode, setInvitationCode] = useState(null);
+    const [timer, setTimer] = useState(300); 
 
+
+    function generateInvitationCode() {
+        const code = Math.random().toString(36).substr(2, 6).toUpperCase();
+        setInvitationCode(code);
+        // Установка таймера на 5 минут для удаления кода
+        setTimer(300);
+    }
+
+    useEffect(() => {
+        let interval;
+        if (timer > 0) {
+            interval = setInterval(() => {
+                setTimer(prevTimer => prevTimer - 1);
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [timer]);
+
+    function formatTime(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    }
 
     function onWillDismiss(ev) {
         if (ev.detail.role === 'confirm') {
@@ -14,58 +42,29 @@ const EmployeesModal = ({ isOpen, onClose}) =>{
     }
     return(
         <IonContent className="ion-padding">
-        <IonModal ref={modal} trigger="open-modal" onWillDismiss={(ev) => onWillDismiss(ev)}>
+        <IonModal id="example-modal" ref={modal} trigger="open-modal" onWillDismiss={(ev) => onWillDismiss(ev)}>
           <IonHeader>
             <IonToolbar>
               <IonButtons slot="start">
                 <IonButton onClick={() => modal.current?.dismiss()}>Отмена</IonButton>
               </IonButtons>
-              <IonTitle>Сотрудник</IonTitle>
-              <IonButtons slot="end">
-                <IonButton strong={true} onClick={() => confirm()}>
-                  Добавить
-                </IonButton>
-              </IonButtons>
+              <IonTitle>Добавить сотрудника</IonTitle>
             </IonToolbar>
           </IonHeader>
           <IonContent className="ion-padding">
-            <IonItem className="custom">
-                <IonSearchbar 
-                    showCancelButton="never"
-                    debounce={500}
-                    class="modal-search"
-                >
-                </IonSearchbar>
+            <IonItem>
+                {invitationCode && (
+                        <>
+                            <IonLabel color='dark'>Код приглашения: {invitationCode}</IonLabel>
+                            <IonText color='dark'>Время действия кода: {formatTime(timer)}</IonText>
+                            <p style={{color: 'black'}}>Отправьте этот код сотруднику для присоединения к вашей crm</p>
+                        </>
+                    )}
             </IonItem>
-            <IonItem className="custom">
-              <IonInput 
-                label="Имя" 
-                labelPlacement="floating" 
-                type="text" 
-                placeholder="Имя"
-                class="custom"
-                 />
+            <IonItem>
+                <IonButton  onClick={() => generateInvitationCode()}>Сгенерировать код приглашения</IonButton>
             </IonItem>
-            <IonItem className="custom">
-              <IonInput
-                label="Телефон"
-                labelPlacement="floating"
-                type="tel"
-                placeholder="888-888-8888"
-                class="custom"
-                
-              />
-            </IonItem>
-            <IonItem className="custom">
-              <IonInput 
-                label="Почта" 
-                labelPlacement="floating" 
-                type="email" 
-                placeholder="email@domain.com"
-                class="custom"
-                 />
-                
-            </IonItem>
+            
           </IonContent>
         </IonModal>
       </IonContent>
