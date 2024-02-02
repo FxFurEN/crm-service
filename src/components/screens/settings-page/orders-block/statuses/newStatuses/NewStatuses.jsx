e7f3c6b2032b29e06309e08d58cc004f92709a36
FxFurEN <nikitostimoshenko@gmail.com>
2024-02-02 10:49:32 +0300

    integration react-colorful

--- src/components/screens/settings-page/orders-block/statuses/newStatuses/NewStatuses.jsx
+++ src/components/screens/settings-page/orders-block/statuses/newStatuses/NewStatuses.jsx
@@ -1,9 +1,11 @@
 import { IonButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonModal, IonText, IonTitle, IonToolbar, IonToast, IonInput, IonCheckbox } from "@ionic/react";
-import { useRef} from "react";
+import { useRef, useState} from "react";
+import { HexColorPicker } from "react-colorful";
 
 import '../../../../../../assets/styles/ion-style.css';
 
 const StatusesModal = ({ isOpen, onClose }) => {
+    const [color, setColor] = useState("#aabbcc");
     const modal = useRef(null);
     
     function onWillDismiss(ev) {
@@ -27,18 +29,23 @@ const StatusesModal = ({ isOpen, onClose }) => {
                         </IonButtons>
                     </IonToolbar>
                 </IonHeader>
-                <IonContent className="ion-padding">
-                    <IonInput 
-                        label="Название" 
-                        labelPlacement="floating" 
-                        fill="outline"
-                    ></IonInput>
+                <IonContent className="ion-padding" >
+                    <br/>
+                        <IonInput 
+                            label="Название" 
+                            labelPlacement="floating" 
+                            fill="outline"
+                        ></IonInput>
                     <br/>
                         <IonCheckbox labelPlacement="end">Закрывает заказ</IonCheckbox>
                     <br/>
                         <IonCheckbox labelPlacement="end">Вызывакт окно оплаты</IonCheckbox>
                     <br/>
                         <IonCheckbox labelPlacement="end">Начисляет зарплатау</IonCheckbox>
+                    <br/>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <HexColorPicker style={{width: '100em'}} color={color} onChange={setColor} />
+                    </div>
                 </IonContent>
             </IonModal>
         </IonContent>
