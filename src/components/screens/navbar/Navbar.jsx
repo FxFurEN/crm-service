8a3b071c3f6bc3e3ba35fe3b85e4d07193aaf612
fxfuren <nikitostimoshenko@gmail.com>
2024-02-06 23:46:25 +0300

    add mobile navbar

--- src/components/screens/navbar/Navbar.jsx
+++ src/components/screens/navbar/Navbar.jsx
@@ -1,16 +1,81 @@
 import './navbar.css';
 import '../../../assets/styles/global.css';
-import { IonIcon } from '@ionic/react';
+import '../../../assets/styles/ion-style.css';
+import { IonIcon, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 import { homeOutline, peopleOutline, settingsOutline, diamondOutline, appsOutline, barChartOutline, archiveOutline, bagHandleOutline} from 'ionicons/icons';
 import { Link, Outlet} from 'react-router-dom';
 import NavHeader from './nav-header/NavHeader';
 import { useSelector } from 'react-redux';
 import { selectVisibility } from '../../../redux/visibilitySlice';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const { isSkladVisible, isMagazinVisible, isOrdersVisible } = useSelector(selectVisibility);
+  const [isMobile, setIsMobile] = useState(false);
 
-  
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth <= 425); 
+    };
+
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+    return () => window.removeEventListener('resize', checkIfMobile);
+  }, []);
+
+  if (isMobile) {
+    return (
+            <IonTabs >
+                  <IonRouterOutlet>
+                      <NavHeader />
+                      <Outlet />
+                  </IonRouterOutlet>
+
+                  <IonTabBar slot="bottom">
+                    <IonTabButton tab="home" href="/home">
+                      <IonIcon icon={homeOutline} />
+                      <span className="link-text">Главная</span>
+                    </IonTabButton>
+
+                    {isOrdersVisible && (
+                      <IonTabButton tab="orders" href="/orders">
+                        <IonIcon icon={archiveOutline} />
+                        <span className="link-text">Заказы</span>
+                      </IonTabButton>
+                    )}
+
+                    <IonTabButton tab="clients" href="/clients">
+                      <IonIcon icon={peopleOutline} />
+                      <span className="link-text">Клиенты</span>
+                    </IonTabButton>
+
+                    <IonTabButton tab="report" href="/report">
+                      <IonIcon icon={barChartOutline} />
+                      <span className="link-text">Отчет</span>
+                    </IonTabButton>
+
+                    {isSkladVisible && (
+                      <IonTabButton tab="inventory" href="/inventory">
+                        <IonIcon icon={appsOutline} />
+                        <span className="link-text">Склад</span>
+                      </IonTabButton>
+                    )}
+
+                    {isMagazinVisible && (
+                      <IonTabButton tab="shop" href="/shop">
+                        <IonIcon icon={bagHandleOutline} />
+                        <span className="link-text">Магазин</span>
+                      </IonTabButton>
+                    )}
+
+                    <IonTabButton tab="settings" href="/settings">
+                      <IonIcon icon={settingsOutline} />
+                      <span className="link-text">Настройки</span>
+                    </IonTabButton>
+                  </IonTabBar>
+              </IonTabs>
+    );
+  }
 
   return (
     <>
