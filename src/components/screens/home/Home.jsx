f3523a408fb0379c9dabe654a06f0bdf75d8c1b5
fxfuren <nikitostimoshenko@gmail.com>
2024-02-08 19:24:48 +0300

    usage lib and-design-charts and delete old lib

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,87 +1,61 @@
 import '../../../assets/styles/main.css';
 import '../../../assets/styles/global.css';
-import '../../../assets/styles/cards-ion.css';
+
+import { useState, useEffect } from 'react';
+import { Card,Flex,} from 'antd';
+import { Area, Column, Line, Pie} from '@ant-design/charts';
 
 
-import sourceData from '../../../data/sourceData.json';
-
-import { Card,Flex } from 'antd';
-
-import {Chart as ChartJS, defaults} from 'chart.js/auto';
-import {Bar, Doughnut, Line} from 'react-chartjs-2';
-
-
-defaults.responsive = true;
-defaults.maintainAspectRatio = false;
 
 const Home = () =>{
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      asyncFetch();
+    }, []);
+  
+    const asyncFetch = () => {
+      fetch('https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json')
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => {
+          console.log('fetch data failed', error);
+        });
+    };
+    const config = {
+      data,
+      xField: 'timePeriod',
+      yField: 'value',
+      xAxis: {
+        range: [0, 1],
+      },
+      height: 400, 
+    width: 600,
+    };
+
     return(
         <main id="main">
-            <Flex wrap="wrap" gap="large">
-                    <div>
-                        <Card title="Bar Chart"> 
-                            <Bar
-                                data={{
-                                    labels: sourceData.map(data => data.label),
-                                    datasets: [
-                                        {
-                                            label: 'Количество',
-                                            data: sourceData.map(data => data.value),
-                                        }
-                                    ],
-                                }}
-                                                                            
-                            />
-                        </Card>
-                    </div>
-                    <div>
-                        <Card title="Line Chart">
-                            <Line
-                                data={{
-                                    labels: sourceData.map(data => data.label),
-                                    datasets: [
-                                        {
-                                            label: 'Количество',
-                                            data: sourceData.map(data => data.value),
-                                        }
-                                    ],
-                                }}
-                                                                            
-                            />
-                        </Card>
-                    </div>
-                    <div>
-                        <Card title="Doughnut Chart">
-                            <Doughnut
-                                data={{
-                                    labels: sourceData.map(data => data.label),
-                                    datasets: [
-                                        {
-                                            label: 'Количество',
-                                            data: sourceData.map(data => data.value),
-                                        }
-                                    ],
-                                }}
-                                                                            
-                            />
-                        </Card>
-                    </div>
-                    <div>
-                        <Card title="Line Chart">
-                            <Line
-                                data={{
-                                    labels: sourceData.map(data => data.label),
-                                    datasets: [
-                                        {
-                                            label: 'Количество',
-                                            data: sourceData.map(data => data.value),
-                                        }
-                                    ],
-                                }}
-                                                                            
-                            />
-                        </Card>
-                    </div>
+            <Flex wrap="wrap" gap="large" justify="center">
+                <div>
+                    <Card size='small' title="Line Chart"> 
+                        <Line {...config} />;
+                    </Card>
+                </div>
+                <div>
+                    <Card size='small' title="Pie Chart"> 
+                        <Pie {...config} />;
+                    </Card>
+                </div>
+                <div>
+                    <Card size='small' title="Column Chart"> 
+                        <Column {...config}/>;
+                    </Card>
+                </div>
+                <div>
+                    <Card size='small' title="Area Chart"> 
+                        <Area {...config}/>;
+                    </Card>
+                </div>
             </Flex>
                 
         </main>
