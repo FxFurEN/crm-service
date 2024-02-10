c2cefc60964922be6f7c66ec02bce562e68b8088
fxfuren <nikitostimoshenko@gmail.com>
2024-02-10 19:44:57 +0300

    del ionic from project

--- src/components/routing/Router.jsx
+++ src/components/routing/Router.jsx
@@ -1,22 +1,7 @@
-import { setupIonicReact, IonApp} from '@ionic/react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from '@store/store.js'; 
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
 
 import Navbar from '@components/layout/navbar/Navbar.jsx';
 import Home from '@pages/Home/Home.jsx';
@@ -46,13 +31,10 @@ import App from '../../App';
 
 
 
-
-setupIonicReact({ mode: 'md' });  
 const Router = () => {
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <IonApp>
             <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Navbar/>} >
@@ -86,7 +68,6 @@ const Router = () => {
                   </Route>
               </Routes>
             </BrowserRouter>
-          </IonApp>
         </PersistGate>
           
       </Provider>
