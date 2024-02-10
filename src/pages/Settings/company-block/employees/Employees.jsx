762bbb6ef2a427e5e6e88b17b553901fce9ee2c8
fxfuren <nikitostimoshenko@gmail.com>
2024-02-11 00:37:16 +0300

    Employees transfer from ant-design

--- src/pages/Settings/company-block/employees/Employees.jsx
+++ src/pages/Settings/company-block/employees/Employees.jsx
@@ -1,7 +1,60 @@
+import {  Table } from 'antd';
+const columns = [
+  {
+    title: 'ФИО',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Должность',
+    dataIndex: 'doljnost',
+  },
+  {
+    title: 'Почта',
+    dataIndex: 'email',
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    doljnost: 'Администратор',
+    email: '123@gmail.com',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    doljnost: 'Менеджер',
+    email: '123@gmail.com',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    doljnost: 'Менеджер',
+    email: '123@gmail.com',
+  },
+];
+
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: (record) => ({
+    disabled: record.name === 'Disabled User',
+    name: record.name,
+  }),
+};
+
 const Employees = () =>{
     return(
         <main id="main">   
-                    
+            <Table
+                rowSelection={{
+                type: 'checkbox',
+                ...rowSelection,
+                }}
+                columns={columns}
+                dataSource={data}
+            />         
         </main>
     )
 }
