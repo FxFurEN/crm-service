7ee56a55ae218cb3fb4d48b4c3ad50915c1482ca
fxfuren <nikitostimoshenko@gmail.com>
2024-02-11 22:37:26 +0300

    GeneralOrders transfer from ant-design

--- src/pages/Settings/orders-block/general-orders/GeneralOrders.jsx
+++ src/pages/Settings/orders-block/general-orders/GeneralOrders.jsx
@@ -1,6 +1,23 @@
+import { Button, Flex, Input } from 'antd';
+const baseStyle = {
+  width: 'clamp(300px, 100%, 500px)',
+  height: 50,
+};
+
+const buttonStyle = {
+    height: 35,
+  };
+        
+
 const GeneralOrders = () =>{
     return(
         <main id="main">
+            <Flex gap="middle" vertical>
+                <Input style={{...baseStyle} } placeholder="Срок по умолчанию, дн*" />
+                <Input style={{...baseStyle} } placeholder="Гарантия по умолчанию, дн*"/>
+                <Button style={{...baseStyle, ...buttonStyle} } type='primary' >Сохранить</Button>
+
+            </Flex>
         </main>
     )
 }
