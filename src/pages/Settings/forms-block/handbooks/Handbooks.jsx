1251c4770d5d6ae3eb24dffa767d53bbf8545495
FxFurEN <nikitostimoshenko@gmail.com>
2024-02-12 10:04:55 +0300

    HandBooks transfer from and-design

--- src/pages/Settings/forms-block/handbooks/Handbooks.jsx
+++ src/pages/Settings/forms-block/handbooks/Handbooks.jsx
@@ -1,10 +1,40 @@
-const Handbooks = () =>{
+import { EditOutlined } from '@ant-design/icons';
+import { Table } from 'antd';
+import { Link } from 'react-router-dom'; // Импортируем Link из react-router-dom
 
-    return(
-        <main id="main">
+const columns = [
+  {
+    title: 'Название',
+    dataIndex: 'name',
+    width: 500,
+    key: 'name',
+  },
+  {
+    title: 'Действие',
+    key: 'operation',
+    width: 100,
+    render: (text, record) => ( 
+      <Link to={`/settings/handbooks/${record.key}`}> 
+        <EditOutlined />
+      </Link>
+    ),
+  },
+];
 
-        </main>
-    )
+const data = [];
+for (let i = 1; i < 5; i++) {
+  data.push({
+    key: i,
+    name: `Справочник ${i}`,
+  });
 }
 
-export default Handbooks
+const Handbooks = () => {
+  return (
+    <main id="main">
+      <Table columns={columns} dataSource={data} />
+    </main>
+  );
+};
+
+export default Handbooks;
