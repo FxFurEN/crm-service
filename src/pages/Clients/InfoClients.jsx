b01cd15a8a9b6c4f316c605443e86a2de73c42bf
FxFurEN <nikitostimoshenko@gmail.com>
2024-02-15 12:26:17 +0300

    redesign InfoClients

--- src/pages/Clients/InfoClients.jsx
+++ src/pages/Clients/InfoClients.jsx
@@ -1,8 +1,10 @@
-import { Modal, Button } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
+import { Modal, Button, List, Typography, ConfigProvider } from 'antd';
 import { useState } from 'react';
 
 const InfoClients = ({ visible, handleOk, handleCancel, client }) => {
     const [confirmLoading, setConfirmLoading] = useState(false);
+    const [customize] = useState(true);
 
     if (!client) {
         return null; 
@@ -14,31 +16,63 @@ const InfoClients = ({ visible, handleOk, handleCancel, client }) => {
             setConfirmLoading(false);
         }, 2000);
     };
-
-    const baseStyle = {
-        width: 'clamp(200px, 100%, 500px)',
-        height: 40,
-    };
+    const customizeRenderEmpty = () => (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '8em',
+          }}
+        >
+          <SmileOutlined
+            style={{
+              fontSize: 20,
+            }}
+          />
+          <p>Данные не найдены</p>
+        </div>
+    );
 
     return (
-        <Modal
-            title="Информация о заказе"
-            centered
-            open={visible}
-            onOk={handleOkAsync}
-            confirmLoading={confirmLoading}
-            onCancel={handleCancel}
-            footer={[
-                <Button key="submit" style={{ ...baseStyle }} loading={confirmLoading} onClick={handleOkAsync}>
-                    Изменить
-                </Button>
-            ]}
-        >
-            <p>Имя: {client.name}</p>
-            <p>Телефон: {client.phone}</p>
-            <p>Почта: {client.email}</p>
-            <p>Тип клиента: {client.clientType === '1' ? 'Физ. лицо' : 'Юр. лицо'}</p>
-        </Modal>
+        <ConfigProvider renderEmpty={customize ? customizeRenderEmpty : undefined}>
+                <Modal
+                    title="Информация о заказе"
+                    centered
+                    open={visible}
+                    onOk={handleOkAsync}
+                    confirmLoading={confirmLoading}
+                    onCancel={handleCancel}
+                    footer={[
+
+                    ]}
+                >
+                    <br/>
+                    <Typography.Text strong>Клиент</Typography.Text>
+                    <List
+                        bordered
+                        dataSource={[
+                            { title: 'Имя', value: client.name },
+                            { title: 'Телефон', value: client.phone },
+                            { title: 'Почта', value: client.email },
+                            { title: 'Тип клиента', value: client.clientType === '1' ? 'Физ. лицо' : 'Юр. лицо' }
+                        ]}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <Typography.Text strong>{item.title}: </Typography.Text>{item.value}
+                            </List.Item>
+                        )}
+                    />
+                    <br/>
+                    <Typography.Text strong>Заказы</Typography.Text>
+                    <List bordered />
+                    <br/>
+                    <Typography.Text strong>Платежи</Typography.Text>
+                    <List bordered/>
+                </Modal>
+        </ConfigProvider>
+        
     );
 };
 
