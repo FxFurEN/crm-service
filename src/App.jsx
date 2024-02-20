2f47f98be9ae98cde7d3c3a073c9f6118597e673
FxFurEN <nikitostimoshenko@gmail.com>
2024-02-20 14:53:08 +0300

    add global configProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { SmileOutlined } from "@ant-design/icons";
 import Router from "@components/routing/Router"
+import { ConfigProvider } from "antd";
+import { useState } from "react";
+
 
 function App() {
-  return <Router/>
+  const [customize] = useState(true);
+  const customizeRenderEmpty = () => (
+    <div
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <SmileOutlined
+        style={{
+          fontSize: 20,
+        }}
+      />
+      <p>Данные не найдены</p>
+    </div>
+  );
+  return (
+    <ConfigProvider renderEmpty={customize ? customizeRenderEmpty : undefined}>
+        <Router/>
+    </ConfigProvider>
+  
+  );
 }
 
 export default App
