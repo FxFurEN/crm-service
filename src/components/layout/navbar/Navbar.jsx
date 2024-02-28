4d671ad14c61d5523f4384dfc27deee93884b179
FxFurEN <nikitostimoshenko@gmail.com>
2024-02-28 13:37:47 +0300

    redesign navbar with ant-design

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -4,90 +4,86 @@ import { Link, Outlet} from 'react-router-dom';
 import NavHeader from '../nav-header/NavHeader';
 import { useSelector } from 'react-redux';
 import { selectVisibility } from '@store/visibilitySlice';
-import { HarmonyOSOutlined, HomeOutlined, InboxOutlined, LineChartOutlined, SettingOutlined, ShoppingCartOutlined, TagsOutlined, TeamOutlined } from '@ant-design/icons'; 
+import { Menu } from 'antd';
+import {
+  HomeOutlined,
+  InboxOutlined,
+  LineChartOutlined,
+  SettingOutlined,
+  ShoppingCartOutlined,
+  TagsOutlined,
+  TeamOutlined
+} from '@ant-design/icons';
+
+const colors = {
+  primary: 'hsla(237, 94%, 81%, 1)',
+  background: 'hsla(257, 11%, 16%, 1)',
+  backgroundSecondary: 'hsla(256, 12%, 12%, 1)',
+  backgroundSecondaryLight: 'hsla(257, 11%, 16%, 1)',
+  textPrimary: 'hsla(0, 0%, 0%, 1)',
+  black: 'hsla(0, 0%, 0%, 1)',
+  white: 'hsla(0, 0%, 100%, 1)',
+  quiteGray: 'hsla(0, 0%, 50%, 1)',
+  grooble: 'hsla(10, 28%, 93%, 1)'
+};
 
 
 const Navbar = () => {
   const { isSkladVisible, isMagazinVisible, isOrdersVisible } = useSelector(selectVisibility);
-
   return (
     <>
-      <nav id="navbar">
-        <ul className="navbar-items flexbox-col">
-          <li className="navbar-logo flexbox">
-            <Link to='/home' className="navbar-item-inner flexbox">
-              <HarmonyOSOutlined style={{ fontSize: '30px', color:'white',}} />
-            </Link>
-          </li>
-          <li className="navbar-item flexbox-left">
-            <Link to='/home' className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <HomeOutlined style={{ fontSize: '20px', color:'white',}}/>
-              </div>
-              <span className="link-text">Главная</span>
-            </Link>
-          </li>
+      <div
+        style={{
+          position: 'absolute',
+          zIndex: 100,
+        }}
+      >
+        <Menu
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            inlineCollapsed={true}
+            style={{
+              overflow: 'auto',
+              height: '100vh',
+              width: '4em',
+              position: 'fixed',
+              backgroundColor: colors.backgroundSecondary, 
+              color: colors.textPrimary, 
+            }}
+          >
+          <Menu.Item key="1" icon={<HomeOutlined />}>
+            <Link to='/home'>Главная</Link>
+          </Menu.Item>
           {isOrdersVisible && (
-          <li className="navbar-item flexbox-left">
-            <Link to='/orders' className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                <TagsOutlined style={{ fontSize: '20px', color:'white',}}/>
-              </div>
-              <span className="link-text">Заказы</span>
-            </Link>
-          </li>
+            <Menu.Item key="2" icon={<TagsOutlined />}>
+              <Link to='/orders'>Заказы</Link>
+            </Menu.Item>
           )}
-          <li className="navbar-item flexbox-left">
-            <Link to='/clients' className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                <TeamOutlined style={{ fontSize: '20px', color:'white',}}/>
-              </div>
-              <span className="link-text">Клиенты</span>
-            </Link>
-          </li>
-          <li className="navbar-item flexbox-left">
-            <Link to='/report' className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <LineChartOutlined style={{ fontSize: '20px', color:'white',}}/>
-              </div>
-              <span className="link-text">Отчет</span>
-            </Link>
-          </li>
+          <Menu.Item key="3" icon={<TeamOutlined />}>
+            <Link to='/clients'>Клиенты</Link>
+          </Menu.Item>
+          <Menu.Item key="4" icon={<LineChartOutlined />}>
+            <Link to='/report'>Отчет</Link>
+          </Menu.Item>
           {isSkladVisible && (
-          <li className="navbar-item flexbox-left">
-            <Link to='/inventory' className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                <InboxOutlined style={{ fontSize: '20px', color:'white',}} />
-              </div>
-              <span className="link-text">Склад</span>
-            </Link>
-          </li>
+            <Menu.Item key="5" icon={<InboxOutlined />}>
+              <Link to='/inventory'>Склад</Link>
+            </Menu.Item>
           )}
           {isMagazinVisible && (
-          <li className="navbar-item flexbox-left">
-            <Link to='/shop' className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <ShoppingCartOutlined style={{ fontSize: '20px', color:'white',}} />
-              </div>
-              <span className="link-text">Магазин</span>
-            </Link>
-          </li>
+            <Menu.Item key="6" icon={<ShoppingCartOutlined />}>
+              <Link to='/shop'>Магазин</Link>
+            </Menu.Item>
           )}
-          <li className="navbar-item flexbox-left">
-            <Link to='/settings' className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                <SettingOutlined style={{ fontSize: '20px', color:'white',}} />
-              </div>
-              <span className="link-text">Настройки</span>
-            </Link>
-          </li>
-
-        </ul>
-      </nav>
+          <Menu.Item key="7" icon={<SettingOutlined />}>
+            <Link to='/settings'>Настройки</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
       <NavHeader/>
       <Outlet />
     </>
-    
   );
 }
 
