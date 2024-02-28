62823c157a8cad1bd08fac7f0cc3679806b2e7f7
fxfuren <nikitostimoshenko@gmail.com>
2024-02-28 18:13:36 +0300

    fix style

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import './navbar.scss';
 import '@assets/styles/global.scss';
-import { Link, Outlet} from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import NavHeader from '../nav-header/NavHeader';
 import { useSelector } from 'react-redux';
 import { selectVisibility } from '@store/visibilitySlice';
 import { Menu } from 'antd';
 import {
+  AimOutlined,
   HomeOutlined,
   InboxOutlined,
   LineChartOutlined,
@@ -27,6 +28,9 @@ const colors = {
   grooble: 'hsla(10, 28%, 93%, 1)'
 };
 
+const iconStyle = {
+  color: '#FFF',
+};
 
 const Navbar = () => {
   const { isSkladVisible, isMagazinVisible, isOrdersVisible } = useSelector(selectVisibility);
@@ -39,49 +43,62 @@ const Navbar = () => {
         }}
       >
         <Menu
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            mode="inline"
-            inlineCollapsed={true}
-            style={{
-              overflow: 'auto',
-              height: '100vh',
-              width: '4em',
-              position: 'fixed',
-              backgroundColor: colors.backgroundSecondary, 
-              color: colors.textPrimary, 
-            }}
-          >
-          <Menu.Item key="1" icon={<HomeOutlined />}>
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={true}
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            width: '4em',
+            position: 'fixed',
+            backgroundColor: colors.backgroundSecondary,
+            color: colors.textPrimary,
+          }}
+        >
+           <div
+          style={{
+            marginTop: '1em',
+            marginBottom: '1em',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center'
+          }}
+        >
+          <AimOutlined style={{ ...iconStyle, fontSize: '24px' }} />
+        </div>
+          <Menu.Item key="1" icon={<HomeOutlined style={iconStyle} />}>
             <Link to='/home'>Главная</Link>
           </Menu.Item>
           {isOrdersVisible && (
-            <Menu.Item key="2" icon={<TagsOutlined />}>
+            <Menu.Item key="2" icon={<TagsOutlined style={iconStyle} />}>
               <Link to='/orders'>Заказы</Link>
             </Menu.Item>
           )}
-          <Menu.Item key="3" icon={<TeamOutlined />}>
+          <Menu.Item key="3" icon={<TeamOutlined style={iconStyle} />}>
             <Link to='/clients'>Клиенты</Link>
           </Menu.Item>
-          <Menu.Item key="4" icon={<LineChartOutlined />}>
+          <Menu.Item key="4" icon={<LineChartOutlined style={iconStyle} />}>
             <Link to='/report'>Отчет</Link>
           </Menu.Item>
           {isSkladVisible && (
-            <Menu.Item key="5" icon={<InboxOutlined />}>
+            <Menu.Item key="5" icon={<InboxOutlined style={iconStyle} />}>
               <Link to='/inventory'>Склад</Link>
             </Menu.Item>
           )}
           {isMagazinVisible && (
-            <Menu.Item key="6" icon={<ShoppingCartOutlined />}>
+            <Menu.Item key="6" icon={<ShoppingCartOutlined style={iconStyle} />}>
               <Link to='/shop'>Магазин</Link>
             </Menu.Item>
           )}
-          <Menu.Item key="7" icon={<SettingOutlined />}>
+          <Menu.Item key="7" icon={<SettingOutlined style={iconStyle} />}>
             <Link to='/settings'>Настройки</Link>
           </Menu.Item>
         </Menu>
       </div>
-      <NavHeader/>
+      <NavHeader />
       <Outlet />
     </>
   );
