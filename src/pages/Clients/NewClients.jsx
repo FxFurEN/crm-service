82f4d790a68e797c2d75ed69baaf725596fb9348
FxFurEN <nikitostimoshenko@gmail.com>
2024-03-06 11:35:19 +0300

    add mask input in modal

--- src/pages/Clients/NewClients.jsx
+++ src/pages/Clients/NewClients.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
-import { Modal, Input, Flex, Select, Button } from 'antd';
+import { Modal, Input, Flex, Button, Radio } from 'antd';
+import InputMask from 'react-input-mask';
 
 const NewClients = ({ visible, handleOk, handleCancel }) => {
     const [confirmLoading, setConfirmLoading] = useState(false);
-    const [clientData, setClientData] = useState({ name: '', phone: '', email: '', clientType: '' });
+    const [clientData, setClientData] = useState({ name: '', phone: '', email: '', initials: '' });
+    const [isLegalEntity, setIsLegalEntity] = useState(false); 
 
     const handleOkAsync = () => {
         setConfirmLoading(true);
-        setTimeout(() => {
+        setTimeout(() => { 
             handleOk(clientData); 
             setConfirmLoading(false);
         }, 2000);
@@ -16,8 +18,7 @@ const NewClients = ({ visible, handleOk, handleCancel }) => {
     const baseStyle = {
         width: 'clamp(200px, 100%, 500px)',
         height: 40,
-      };
-
+    };
 
     const handleChange = (field, value) => {
         setClientData({ ...clientData, [field]: value });
@@ -32,43 +33,60 @@ const NewClients = ({ visible, handleOk, handleCancel }) => {
             confirmLoading={confirmLoading}
             onCancel={handleCancel}
             footer={[
-                <Button key="submit" style={{...baseStyle}} loading={confirmLoading} onClick={handleOkAsync}>
-                  Добавить
+                <Button key="submit" style={{ ...baseStyle }} loading={confirmLoading} onClick={handleOkAsync}>
+                    Добавить
                 </Button>
-              ]}
+            ]}
         >
             <Flex vertical gap={20}>
-                <Input
-                    style={{...baseStyle} }
-                    placeholder="Имя"
-                    value={clientData.name}
-                    onChange={(e) => handleChange('name', e.target.value)}
-                />
-                <Input
-                    style={{...baseStyle} }
-                    placeholder="Телефон"
+                <Flex horizontal>
+                    <Radio.Group onChange={(e) => setIsLegalEntity(e.target.value)} value={isLegalEntity}>
+                        <Radio value={false}>Физ. лицо</Radio>
+                        <Radio value={true}>Юр.лицо</Radio>
+                    </Radio.Group>
+                </Flex>
+                {isLegalEntity ? (
+                    <>
+                        <Input
+                            style={{ ...baseStyle }}
+                            placeholder="Название организации"
+                            value={clientData.name}
+                            onChange={(e) => handleChange('name', e.target.value)}
+                        />
+                        <Input
+                            count={{
+                                show: true,
+                                max: 9,
+                              }}
+                            maxLength={9}
+                            style={{ ...baseStyle }}
+                            placeholder="УНП"
+                            value={clientData.inn}
+                            onChange={(e) => handleChange('inn', e.target.value)}
+                        />
+                    </>
+                ) : (
+                    <Input
+                        style={{ ...baseStyle }}
+                        placeholder="ФИО"
+                        value={clientData.initials}
+                        onChange={(e) => handleChange('initials', e.target.value)}
+                    />
+                )}
+                <InputMask
+                    mask="+375 (99) 999-99-99"
                     value={clientData.phone}
                     onChange={(e) => handleChange('phone', e.target.value)}
-                />
+                >
+                    {(inputProps) => <Input {...inputProps} style={{ ...baseStyle }} placeholder="Телефон" />}
+                </InputMask>
                 <Input
-                    style={{...baseStyle} }
+                    style={{ ...baseStyle }}
                     placeholder="Почта"
                     value={clientData.email}
                     onChange={(e) => handleChange('email', e.target.value)}
                 />
-                <Select
-                    style={{...baseStyle}}
-                    placeholder="Тип клиента"
-                    onChange={handleChange}
-                    options={[
-                        { value: '1', label: 'Физ. лицо' },
-                        { value: '2', label: 'Юр. лицо' },
-                    ]}
-                />
-                <br/>
-               
             </Flex>
-            
         </Modal>
     );
 };
