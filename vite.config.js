e12dc8a9976715c72bb36262595e9e82ea1d06f8
FxFurEN <nikitostimoshenko@gmail.com>
2024-03-06 08:19:30 +0300

    del env in client side

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_TINYMCE_API_KEY':JSON.stringify(process.env.VITE_TINYMCE_API_KEY)
-  },
   resolve: {
     alias: {
       '@components': '/src/components',
