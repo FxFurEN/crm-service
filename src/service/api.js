48da0759c871269ef2e1d4f76420fdc6089d5c32
fxfuren <nikitostimoshenko@gmail.com>
2024-03-06 19:17:07 +0300

    fix api

--- src/service/api.js
+++ src/service/api.js
@@ -1,36 +1,11 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:3300"
+    baseURL: "http://localhost:3001"
 })
 
 export const crmAPI = {
-    getAllOrders() {
-      return instance.get('/crm/loadOrders');
-    },
-    addOrder(formData) {
-      return instance.post('/crm/addOrder', formData);
-    },
-   
     getAllClientsData() {
-        return instance.get('/crm/loadClientsData');
+        return instance.get('/clientsData');
       },
-    addClient(formData) {
-        return instance.post('/crm/addClient', formData);
-    },
-    loadClientTypes() {
-        return instance.get('/crm/loadClientTypes');
-    },
-    loadCategories() {
-        return instance.get('/crm/loadCategories');
-    },
-    addCategory(formData) {
-        return instance.post('/crm/addCategory', formData);
-    },
-    loadGoods() {
-        return instance.get('/crm/loadGoods');
-    },
-    addGoods(formData) {
-        return instance.post('/crm/addGoods', formData);
-    },
 }
