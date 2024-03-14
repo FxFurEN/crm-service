ebcb8d458f4b45f431dcc0d39321121fc52d5c8d
FxFurEN <nikitostimoshenko@gmail.com>
2024-03-14 13:33:09 +0300

    test veiw data from db in chart

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,62 +1,67 @@
 import { useState, useEffect } from 'react';
-import { Card,Flex,} from 'antd';
-import { Area, Column, Line, Pie} from '@ant-design/charts';
+import { Card, Flex } from 'antd';
+import { Area, Column, Line, Pie } from '@ant-design/charts';
+import { crmAPI } from '@service/api';
 
+const Home = () => {
+  const [services, setServices] = useState([]);
 
+  useEffect(() => {
+    asyncFetchServices();
+  }, []);
 
-const Home = () =>{
-    const [data, setData] = useState([]);
+  const asyncFetchServices = () => {
+    crmAPI.getAllServices()
+      .then(response => {
+        const servicesData = response.data;
+        setServices(servicesData);
+      })
+      .catch(error => {
+        console.error('Failed to fetch services:', error);
+      });
+  };
 
-    useEffect(() => {
-      asyncFetch();
-    }, []);
-  
-    const asyncFetch = () => {
-      fetch('https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json')
-        .then((response) => response.json())
-        .then((json) => setData(json))
-        .catch((error) => {
-          console.log('fetch data failed', error);
-        });
-    };
-    const config = {
-      data,
-      xField: 'timePeriod',
-      yField: 'value',
-      xAxis: {
-        range: [0, 1],
-      },
-      height: 400, 
+  const priceChartData = services.map(service => ({
+    name: service.name,
+    price: service.price,
+  }));
+
+  const config = {
+    data: priceChartData,
+    xField: 'name',
+    yField: 'price',
+    xAxis: { label: { autoRotate: false } },
+    tooltip: { formatter: ({ value }) => `$${value.toFixed(2)}` },
+    height: 400,
     width: 600,
-    };
+  };
 
-    return(
-        <main id="main">
-            <Flex wrap="wrap" gap="large" justify="center">
-                <div>
-                    <Card size='small' title="Line Chart"> 
-                        <Line {...config} />;
-                    </Card>
-                </div>
-                <div>
-                    <Card size='small' title="Pie Chart"> 
-                        <Pie {...config} />;
-                    </Card>
-                </div>
-                <div>
-                    <Card size='small' title="Column Chart"> 
-                        <Column {...config}/>;
-                    </Card>
-                </div>
-                <div>
-                    <Card size='small' title="Area Chart"> 
-                        <Area {...config}/>;
-                    </Card>
-                </div>
-            </Flex>
-                
-        </main>
-       
-    )
-}
-export default Home
+  return (
+    <main id="main">
+      <Flex wrap="wrap" gap="large" justify="center">
+        <div>
+          <Card size="small" title="Line Chart">
+            <Line {...config} />
+          </Card>
+        </div>
+        <div>
+          <Card size="small" title="Pie Chart">
+            <Pie {...config} />
+          </Card>
+        </div>
+        <div>
+          <Card size="small" title="Column Chart">
+            <Column {...config} />
+          </Card>
+        </div>
+        <div>
+          <Card size="small" title="Area Chart">
+            <Area {...config} />
+          </Card>
+        </div>
+      </Flex>
+    </main>
+  );
+};
+
+export default Home;
