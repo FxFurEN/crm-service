185cadd0c80d1dfb9ae136d23c4561246c32909e
FxFurEN <nikitostimoshenko@gmail.com>
2024-03-15 13:25:58 +0300

    redesign modal

--- src/pages/Orders/InfoOrders.jsx
+++ src/pages/Orders/InfoOrders.jsx
@@ -1,4 +1,4 @@
-import { Modal, List, Typography, Table, Flex, Tag } from 'antd';
+import { Modal, List, Typography, Table, Flex, Tag, Row, Col } from 'antd';
 import { useState } from 'react';
 
 const InfoOrders = ({ visible, handleOk, handleCancel, order }) => {
@@ -30,29 +30,37 @@ const InfoOrders = ({ visible, handleOk, handleCancel, order }) => {
   ];
 
   const data = [
-    { title: 'Товар', value: order.goods },
-    { title: 'Дата создания', value: order.updateDate },
-    { title: 'Статус', value: <Tag>{order.status}</Tag> },
+    { title: 'Услуга', value: order.service },
+    { title: 'Дата создания', value: order.createdAt },
+    { title: 'Статус', value: <Tag>пока нету</Tag> },
   ];
   const dataClient = [
-    { title: 'Клиент', value: order.nameClient },
-    { title: 'Телефон', value: order.phoneClient },
-    { title: 'Почта', value: order.emailClient },
+    { title: 'Клиент', value: order.client },
+    { title: 'Телефон', value: order.client },
+    { title: 'Почта', value: order.client },
   ];
   const dataAdditional = [
-    { title: 'Тип заказа', value: order.typeOrder },
-    { title: 'Менеджер', value: order.menager },
-    { title: 'Исполнить', value: order.performer },
-    { title: 'Срок', value: order.deadLine },
+    { title: 'Сотрудник', value: order.employee },
+    { title: 'Срок', value: order.leadTime },
+    { title: 'Комментарий к заказу', value: order.comments },
   ];
   const historyData = [
     { title: 'Изменение 1', date: '2024-02-14', description: 'Описание изменения 1' },
     { title: 'Изменение 2', date: '2024-02-15', description: 'Описание изменения 2' },
+    { title: 'Изменение 3', date: '2024-02-14', description: 'Описание изменения 3' },
+    { title: 'Изменение 4', date: '2024-02-15', description: 'Описание изменения 4' },
+    { title: 'Изменение 5', date: '2024-02-14', description: 'Описание изменения 5' },
+    { title: 'Изменение 6', date: '2024-02-15', description: 'Описание изменения 6' },
+    { title: 'Изменение 7', date: '2024-02-15', description: 'Описание изменения 7' },
+    { title: 'Изменение 8', date: '2024-02-15', description: 'Описание изменения 8' },
+    { title: 'Изменение 9', date: '2024-02-15', description: 'Описание изменения 9' },
+    { title: 'Изменение 10', date: '2024-02-15', description: 'Описание изменения 10' },
+    { title: 'Изменение 11', date: '2024-02-15', description: 'Описание изменения 11' },
   ];
 
   return (
       <Modal
-        title={`Заказ № ${order?.number}`}
+        title={`Заказ № ${order.key}`}
         centered
         open={visible}
         onOk={handleOkAsync}
@@ -61,62 +69,62 @@ const InfoOrders = ({ visible, handleOk, handleCancel, order }) => {
         width={1000}
         footer={[]}
       >
-        <Typography.Text strong>Товары и услуги</Typography.Text>
-        <Table columns={columns} dataSource={[]} bordered />
-        <br />
-        <Typography.Text strong>Платежи</Typography.Text>
-        <Table columns={columns} dataSource={[]} bordered />
-        <br />
-        <Flex wrap="wrap" gap="large" direction="row">
-          <div style={{ flex: 1, minWidth: 200, maxWidth: '100%' }}>
-            <Typography.Text strong>Информация о заказе</Typography.Text>
-            <List
-              dataSource={data}
-              renderItem={(item) => (
-                <List.Item>
-                  <Typography.Text strong>{item.title}: </Typography.Text>
-                  {item.value}
-                </List.Item>
-              )}
-            />
-          </div>
-          <div style={{ flex: 1, minWidth: 200, maxWidth: '100%' }}>
-            <Typography.Text strong>Клиент</Typography.Text>
-            <List
-              dataSource={dataClient}
-              renderItem={(item) => (
-                <List.Item>
-                  <Typography.Text strong>{item.title}: </Typography.Text>
-                  {item.value}
-                </List.Item>
-              )}
-            />
-          </div>
-          <div style={{ flex: 1, minWidth: 200, maxWidth: '100%' }}>
-            <Typography.Text strong>Дополнительно</Typography.Text>
-            <List
-              dataSource={dataAdditional}
-              renderItem={(item) => (
-                <List.Item>
-                  <Typography.Text strong>{item.title}: </Typography.Text>
-                  {item.value}
-                </List.Item>
-              )}
-            />
-          </div>
-        </Flex>
-        <div>
-          <Typography.Title level={3}>История изменений</Typography.Title>
-          <List
-            bordered
-            dataSource={historyData}
-            renderItem={(item) => (
-              <List.Item>
-                <Typography.Text strong>{item.title}:</Typography.Text> {item.date} - {item.description}
-              </List.Item>
-            )}
-          />
-        </div>
+        
+        <Row gutter={[20, 20]}>
+            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+              <div>
+                <Typography.Title level={3}>История изменений</Typography.Title>
+                <List
+                  bordered
+                  dataSource={historyData}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <Typography.Text strong>{item.title}:</Typography.Text> {item.date} - {item.description}
+                    </List.Item>
+                  )}
+                  style={{ maxHeight: '40em', overflowY: 'auto' }}
+                />
+              </div>
+            </Col>
+            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                <div style={{ flex: 1, minWidth: 200, maxWidth: '100%' }}>
+                  <Typography.Title level={3}>Информация о заказе</Typography.Title>
+                  <List
+                    dataSource={data}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <Typography.Text strong>{item.title}: </Typography.Text>
+                        {item.value}
+                      </List.Item>
+                    )}
+                  />
+                </div>
+                <div style={{ flex: 1, minWidth: 200, maxWidth: '100%' }}>
+                  <Typography.Title level={3}>Клиент</Typography.Title>
+                  <List
+                    dataSource={dataClient}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <Typography.Text strong>{item.title}: </Typography.Text>
+                        {item.value}
+                      </List.Item>
+                    )}
+                  />
+                </div>
+                <div style={{ flex: 1, minWidth: 200, maxWidth: '100%' }}>
+                  <Typography.Title level={3}>Дополнительно</Typography.Title>
+                  <List
+                    dataSource={dataAdditional}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <Typography.Text strong>{item.title}: </Typography.Text>
+                        {item.value}
+                      </List.Item>
+                    )}
+                  />
+                </div>
+            </Col>
+        </Row>
       </Modal>
 
   );
