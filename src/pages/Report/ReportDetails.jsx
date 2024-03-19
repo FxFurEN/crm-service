0e7fe1e12d2fd99aff48cde42741744c60c521ad
FxFurEN <nikitostimoshenko@gmail.com>
2024-03-19 09:30:52 +0300

    fix view data in table

--- src/pages/Report/ReportDetails.jsx
+++ src/pages/Report/ReportDetails.jsx
@@ -1,36 +1,53 @@
-import { useState, useEffect } from 'react';
-import { Spin, Table } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Spin, Table, Select } from 'antd';
 import { crmAPI } from '@service/api';
+import dayjs from 'dayjs';
+
+const { Option } = Select;
 
 const ReportDetails = ({ reportType }) => {
     const [dataSource, setDataSource] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [uniqueCategories, setUniqueCategories] = useState([]);
 
     const fetchReportData = async (type) => {
         setLoading(true);
         try {
-            let data;
+            let response;
             switch (type) {
                 case 'Отчет о заказах по категориям услуг':
-                    data = await crmAPI.getOrdersByCategory();
+                    response = await crmAPI.getOrdersByCategory();
                     break;
                 case 'Отчет о выполненных заказах сотрудниками':
-                    data = await crmAPI.getCompletedOrdersByEmployee();
+                    response = await crmAPI.getCompletedOrdersByEmployee();
                     break;
                 case 'Отчет о клиентах с наибольшим числом заказов':
-                    data = await crmAPI.getClientsWithMostOrders();
+                    response = await crmAPI.getClientsWithMostOrders();
                     break;
                 case 'Отчет о стадиях выполнения заказов':
-                    data = await crmAPI.getOrderStages();
+                    response = await crmAPI.getOrderStages();
                     break;
                 case 'Отчет о среднем времени выполнения заказов':
-                    data = await crmAPI.getAverageOrderCompletionTime();
+                    response = await crmAPI.getAverageOrderCompletionTime();
                     break;
                 default:
-                    data = [];
+                    response = { data: [] };
                     break;
             }
-            setDataSource(data);
+            const dataWithIndex = response.data.map((item, index) => ({
+                ...item,
+                index: index + 1,
+                service: item.service.name, 
+                category: item.service.category.name,
+                createdAt: dayjs(item.createdAt).format('DD.MM.YYYY'), 
+                leadTime: dayjs(item.leadTime).format('DD.MM.YYYY'),
+                client: `${item.client.initials ? item.client.initials + ' ' : ''}${item.client.name ? item.client.name : ''}`,
+            }));
+            setDataSource(dataWithIndex);
+            // Получение уникальных категорий
+            const uniqueCategories = [...new Set(dataWithIndex.map(item => item.category))];
+            setUniqueCategories(uniqueCategories);
         } catch (error) {
             console.error('Ошибка получения данных отчета:', error);
         } finally {
@@ -44,18 +61,61 @@ const ReportDetails = ({ reportType }) => {
         }
     }, [reportType]);
 
-    const columns = dataSource.length > 0 ? Object.keys(dataSource[0]).map(key => ({
-        title: key,
-        dataIndex: key,
-        key,
-    })) : [];
+    const handleCategoryChange = (value) => {
+        setSelectedCategory(value);
+    };
+
+    const filteredData = selectedCategory ? dataSource.filter(item => item.category === selectedCategory) : dataSource;
+
+    const columns = filteredData.length > 0 ? [
+        {
+            title: '№',
+            dataIndex: 'index',
+            key: 'index',
+        },
+        {
+            title: 'Название услуги',
+            dataIndex: 'service',
+            key: 'service',
+        },
+        {
+            title: 'Клиент',
+            dataIndex: 'client', 
+            key: 'client',
+        },
+        {
+            title: 'Дата создания',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+        },
+        {
+            title: 'Срок выполнения',
+            dataIndex: 'leadTime',
+            key: 'leadTime',
+        }
+    ] : [];
 
     return (
         <div>
+            <Select showSearch placeholder="Выберите категорию" style={{ width: 200, marginBottom: 16 }} onChange={handleCategoryChange}>
+                <Option value="">Все категории</Option>
+                {uniqueCategories.map(category => (
+                    <Option key={category} value={category}>{category}</Option>
+                ))}
+            </Select>
             {loading ? (
                 <Spin />
             ) : (
-                <Table dataSource={dataSource} columns={columns} />
+                <Table 
+                    dataSource={filteredData}
+                    pagination={{
+                        pageSize: 50,
+                        position: ['none'],
+                    }}
+                    scroll={{
+                        y: 600,
+                    }}
+                    columns={columns} />
             )}
         </div>
     );
